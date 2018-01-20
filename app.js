03d28ad1f2831d31e607419586d63a2a5baa34ba
Harish Krishnan <harishkrishnan1993@gmail.com>
2018-01-20 20:45:52 +0530

    Added product list component

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 class ProductList extends React.Component {
   render() {
     return (
-      <div className="ui unstackable items"></div>
+      <div className="ui unstackable items">
+        Hello, friend! I am a basic React Component
+      </div>
     );
   }
 }
+
+ReactDOM.render(<ProductList />, document.getElementById("content"));
